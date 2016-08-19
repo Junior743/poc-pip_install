2b2f2230c95d3c7f24c2ca3c05a5d4f56a57c6eb
Junior <carloshenriquesousajunior@gmail.com>
2016-08-19 01:44:06 -0300

    Alteracoes no setup.py

--- setup.py
+++ setup.py
@@ -29,10 +29,6 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'dir2pi = libpip2pi.commands:dir2pi',
-            'pip2pi = libpip2pi.commands:pip2pi',
-            'pip2tgz = libpip2pi.commands:pip2tgz',
-            'pip2whl = libpip2pi.commands:pip2whl',
         ],
     },
     install_requires=[
