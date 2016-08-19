4a867b91275f8d506a266e5fdb0265899b8e1461
Junior <carloshenriquesousajunior@gmail.com>
2016-08-19 00:17:47 -0300

    Implementacao do setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='PipInstall',
+      version='1.0',
+      description='poc para instalar ambiente strike via pip',
+      author='Junior',
+      author_email='carlos.junior@tevec.com.br',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
+
