d96bfcc91415cddb5e47bb76f1f63a760a6c2ad9
Junior <carloshenriquesousajunior@gmail.com>
2016-08-19 01:57:24 -0300

    Implementacao da classe principal

--- python/teste.py
+++ python/teste.py
@@ -1,1 +1,11 @@
 import os
+
+class Principal(Object):
+
+    def __init__(self, valor):
+        print(calculaValor(valor))
+        
+    def calculaValor(valor):
+        valor = valor + 10
+        return valor
+
