9752b485bc8d38ba4df14a27bccbaa3e29c9a826
Junior <carloshenriquesousajunior@gmail.com>
2016-09-16 17:22:19 -0300

    setup.py 2

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email="carlos.junior@tevec.com.br",
     install_requires=[
         "numpy",
+        "-e git+https://github.com/Junior743/poc-travis/tarball/master#egg=poc-travis-1.0.0",
     ],
     dependency_links = ['https://github.com/Junior743/poc-travis/tarball/master#egg=poc-travis-1.0.0'],
 )
